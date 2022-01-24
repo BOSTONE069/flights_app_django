78b8142cffd6491c3bc9829dc9eb68909616ba59
unknown <bostoneochieng69@gmail.com>
2022-01-24 15:56:42 +0300

    update django framework for the flight app

--- airline/urls.py
+++ airline/urls.py
@@ -18,7 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("flights/", include("flights.urls")) #this is to include the app component in the project
+    path("flights/", include("flights.urls")), #this is to include the app component in the project
     path("users/", include("users.urls"))
 ]
 
